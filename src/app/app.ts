4b2d138b93e90291bba69c8a2614a43242e16539
salak <128145015+mamedsk@users.noreply.github.com>
2026-03-30 23:49:36 +0100

    creating task page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Task } from './task/task';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Task],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('taskManager');
